683fabe18026f2c10024c674cff75420d16ea7e0
SUMIT SHUKLA <shuklasumiit.2005@gmail.com>
2026-09-12 09:40:13 +0530

    Time: 13 ms (11.7%), Space: 12.9 MB (6.93%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -0,0 +1,35 @@
+int dp[505][505];
+
+int fun(int p, int i, vector<int>& nums, int m, int k) {
+    if(i >= nums.size()) return 0;
+
+    if(dp[p][i] != -1)
+        return dp[p][i];
+
+    int ans = 0;
+
+    m = max(m, nums[i]);
+    int len = i - p + 1;
+
+    if(len == k) {
+        int c1 = (m * len) + fun(i + 1, i + 1, nums, 0, k);
+        ans = max(ans, c1);
+    }
+    else {
+        int c1 = (m * len) + fun(i + 1, i + 1, nums, 0, k);
+        int c2 = fun(p, i + 1, nums, m, k);
+
+        ans = max(ans, c1);
+        ans = max(ans, c2);
+    }
+
+    return dp[p][i] = ans;
+}
+
+class Solution {
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        memset(dp, -1, sizeof(dp));
+        return fun(0, 0, arr, 0, k);
+    }
+};
